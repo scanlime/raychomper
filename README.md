e18513891bc0b0adba6bb24009768093634b5b92
Micah Elizabeth Scott <beth@scanlime.org>
2013-04-08 12:42:04 -0700

    Histogram plotting

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-raychomper
+RayChomper
 ==========
 
-Experimental raytracing toy
+Experimental raytracing toy!
+
+This is a simple stochastic path tracer, with a real-time histogram view which shows the density of light at every single point in space.
+
+Micah Elizabeth Scott <micah@scanlime.org>
--- src/testApp.cpp
+++ src/testApp.cpp
@@ -1,61 +1,93 @@
+/*
+ * This file is part of the RayChomper experimental raytracer. 
+ * Copyright (c) 2013 Micah Elizabeth Scott <micah@scanlime.org>
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 #include "testApp.h"
 
-//--------------------------------------------------------------
-void testApp::setup(){
-
+void testApp::setup()
+{
+    ofSetWindowTitle("Raychomper");
 }
 
-//--------------------------------------------------------------
-void testApp::update(){
-
+void testApp::update()
+{
 }
 
-//--------------------------------------------------------------
-void testApp::draw(){
+void testApp::draw()
+{
+    ofPoint p1, p2;
 
+    p1.set(ofGetMouseX(), ofGetMouseY());
+
+    hist.clear();
+
+    for (unsigned i = 0; i < 10000; ++i) {
+        p2.set(ofRandom(100, 200), ofRandom(100, 200));
+        hist.line(p1, p2);
+    }
+    hist.increment();
+
+    hist.render(histPixels, 0.01);
+    histTexture.loadData(histPixels);
+    histTexture.draw(0, 0);
 }
 
-//--------------------------------------------------------------
-void testApp::keyPressed(int key){
-
+void testApp::keyPressed(int key)
+{
 }
 
-//--------------------------------------------------------------
-void testApp::keyReleased(int key){
-
+void testApp::keyReleased(int key)
+{
 }
 
-//--------------------------------------------------------------
-void testApp::mouseMoved(int x, int y ){
-
+void testApp::mouseMoved(int x, int y )
+{
 }
 
-//--------------------------------------------------------------
-void testApp::mouseDragged(int x, int y, int button){
-
+void testApp::mouseDragged(int x, int y, int button)
+{
 }
 
-//--------------------------------------------------------------
-void testApp::mousePressed(int x, int y, int button){
-
+void testApp::mousePressed(int x, int y, int button)
+{
 }
 
-//--------------------------------------------------------------
-void testApp::mouseReleased(int x, int y, int button){
-
+void testApp::mouseReleased(int x, int y, int button)
+{
 }
 
-//--------------------------------------------------------------
-void testApp::windowResized(int w, int h){
-
+void testApp::windowResized(int w, int h)
+{
+    hist.resize(w, h);
+    histTexture.allocate(w, h, GL_LUMINANCE);
 }
 
-//--------------------------------------------------------------
-void testApp::gotMessage(ofMessage msg){
-
+void testApp::gotMessage(ofMessage msg)
+{
 }
 
-//--------------------------------------------------------------
-void testApp::dragEvent(ofDragInfo dragInfo){ 
-
+void testApp::dragEvent(ofDragInfo dragInfo)
+{ 
 }
